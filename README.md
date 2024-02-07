8ce1ce4e23136e9808172ef9740edd3251c87235
Tettei Shahday-Annang <tetteis@gmail.com>
2024-02-07 10:28:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Update a file through a Python algorithm 👩‍💻
+
+## 📝 Description
+
+This Python script is designed to manage a list of IP addresses in a text file (`allow_list.txt`). It provides functionality to remove specific IP addresses from the list of allowed addresses.
+
+## 🧑‍🏫 Instructions
+
+1. **Setup**: Place the `allow_list.txt` file in the same directory as the Python script.
+2. **Configuration**:
+   - Modify the `import_file` variable in the script to match the name of your text file.
+   - Update the `remove_list` variable with the IP addresses that need to be removed from the list.
+3. **Execution**:
+   - Run the Python script.
+   - The script will read the initial contents of the file, remove the specified IP addresses, and rewrite the file with the updated list.
+
+## 📃 License
+
+Licensed under the MIT License, Copyright © 2024
---
+++ update-a-file-algorithm.py
@@ -0,0 +1,52 @@
+# Assign `import_file` to the name of the file
+
+import_file = "allow_list.txt"
+
+# Assign `remove_list` to a list of IP addresses that are no longer allowed to access restricted information.
+
+remove_list = ["192.168.97.225", "192.168.158.170", "192.168.201.40", "192.168.58.57"]
+
+# Build `with` statement to read in the initial contents of the file
+
+with open(import_file, "r") as file:
+
+    # Use `.read()` to read the imported file and store it in a variable named `ip_addresses`
+
+    ip_addresses = file.read()
+
+# Use `.split()` to convert `ip_addresses` from a string to a list
+
+ip_addresses = ip_addresses.split()
+
+# Build iterative statement
+# Name loop variable `element`
+# Loop through `remove_list`
+
+for element in remove_list:
+
+    # Create conditional statement to evaluate if `element` is in `ip_addresses`
+
+    if element in ip_addresses:
+
+        # use the `.remove()` method to remove
+        # elements from `ip_addresses`
+
+        ip_addresses.remove(element)
+
+        # print("Reading ip addresses after removing:", ip_addresses)
+
+# Convert `ip_addresses` back to a string so that it can be written into the text file
+
+ip_addresses = "\n".join(ip_addresses)
+
+# Build `with` statement to rewrite the original file
+
+with open(import_file, "w") as file:
+
+    # Rewrite the file, replacing its content with `ip_addresses`
+
+    file.write(ip_addresses)
+
+# Output the updated `ip_addresses` list
+
+print(ip_addresses)
